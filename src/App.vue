f2e4998dc568b1aba694f474b247320ee2c2d5a3
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-05-03 19:08:56 +0300

    Implement MButton

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <script>
-export default {}
+import MButton from '@/components/MButton.vue'
+
+export default {
+  components: { MButton },
+
+  data() {
+    return {
+      users: [],
+    }
+  },
+
+  computed: {
+    isButtonDisabled() {
+      return this.users.length >= 5
+    },
+  },
+}
 </script>
 
 <template>
-  <div><h1>Hello Vue</h1></div>
+  <div>
+    <h1>Hello Vue</h1>
+    {{ users }}
+    {{ isButtonDisabled }}
+  </div>
+  <div>
+    <MButton :disabled="isButtonDisabled" @click="users.push({})" />
+    <MButton />
+  </div>
 </template>
---
+++ src/components/MButton.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  props: ['disabled'],
+}
+</script>
+
+<template>
+  <a class="waves-effect waves-light btn" :class="{ disabled: disabled }">
+    button
+  </a>
+</template>
+
+<style scoped>
+a {
+  margin: 5px;
+}
+</style>
