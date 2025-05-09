f8ae0dbb438510fb5feb6061c9161fecf95d002b
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-05-09 18:27:26 +0300

    Refactored: MButton

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import MButton from '@/components/MButton.vue'
 import MSwitch from '@/components/MSwitch.vue'
 import MFAB from './components/MFAB.vue'
 import MDropdown from './components/MDropdown.vue'
-import MButtonCloud from './components/MButtonCloud.vue'
+
 export default {
-  components: { MButton, MSwitch, MBreadcrumbs, MFAB, MDropdown, MButtonCloud },
+  components: { MButton, MSwitch, MBreadcrumbs, MFAB, MDropdown },
 
   data() {
     return {
@@ -41,16 +41,27 @@ export default {
       
     </article>
 
-    <article>
-      <MFAB icon="home" v-model="hasNewMessage" :is-show="isLoggedIn" />
-    </article> -->
+  -->
 
-    <MSwitch v-model="isLoggedIn" />
+    <!-- 
     <article>
       <MDropdown v-if="isLoggedIn" />
     </article>
     <article>
       <MButtonCloud />
+    </article> -->
+
+    <article>
+      <MButton size="small" caption="Удалить" icon="remove" />
+      <MButton caption="Добавить" icon="add" :isIconRight="true" />
+      <MButton size="large" caption="Создать" icon="create" />
+    </article>
+    <article></article>
+    <article>
+      <MSwitch v-model="isLoggedIn" />
+    </article>
+    <article>
+      <MFAB icon="home" v-model="hasNewMessage" :is-show="isLoggedIn" />
     </article>
   </section>
 </template>
--- src/components/MButton.vue
+++ src/components/MButton.vue
@@ -1,12 +1,22 @@
 <script>
 export default {
-  props: ['isDisabled'],
+  props: ['isDisabled', 'size', 'caption', 'icon', 'isIconRight'],
 }
 </script>
 
 <template>
-  <a class="waves-effect waves-light btn" :class="{ disabled: isDisabled }">
-    button
+  <a
+    class="waves-effect waves-light btn"
+    :class="{
+      disabled: isDisabled,
+      'btn-small': size === 'small',
+      'btn-large': size === 'large',
+    }"
+  >
+    <i class="material-icons" :class="isIconRight ? 'right' : 'left'">
+      {{ icon }}
+    </i>
+    {{ caption }}
   </a>
 </template>
 
