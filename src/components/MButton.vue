a6e8e14b3816b2e2dcc712b48138ac4e1aa08fb5
Yuriyzhdanov <yuuriyzhdanov@gmail.com>
2025-05-10 18:52:08 +0300

    fixed animation

--- src/components/MButton.vue
+++ src/components/MButton.vue
@@ -9,45 +9,32 @@ export default {
   },
   mounted() {
     console.log('MButtons mounted')
-    setTimeout(() => {
+    requestAnimationFrame(() => {
       this.isInvisible = false
-    }, 1000)
+    })
   },
 }
 </script>
 
 <template>
-  <transition name="fade">
-    <a
-      v-show="!isInvisible"
-      class="waves-effect waves-light btn"
-      :class="{
-        disabled: isDisabled,
-        'btn-small': size === 'small',
-        'btn-large': size === 'large',
-      }"
-    >
-      <i class="material-icons" :class="isIconRight ? 'right' : 'left'">
-        {{ icon }}
-      </i>
-      {{ caption }}
-    </a>
-  </transition>
+  <a
+    class="waves-effect waves-light btn scale-transition"
+    :class="{
+      disabled: isDisabled,
+      'btn-small': size === 'small',
+      'btn-large': size === 'large',
+      'scale-out': isInvisible,
+    }"
+  >
+    <i class="material-icons" :class="isIconRight ? 'right' : 'left'">
+      {{ icon }}
+    </i>
+    {{ caption }}
+  </a>
 </template>
 
 <style scoped>
 a {
   margin: 5px;
-}
-.waves-effect {
-  transition: all 0.3s ease;
-}
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
 }
 </style>
